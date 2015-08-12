7f16a0f7345f59eae986037dcfa9c6ed766f3435
xxr <xxr@gmail.com>
2015-08-12 17:56:56 +0800

    新增数据库帮助类以及各表对应的实体类。

---
+++ app/src/main/java/com/xxr/coolweather/model/City.java
@@ -0,0 +1,44 @@
+package com.xxr.coolweather.model;
+
+/**
+ * Created by Administrator on 2015/8/12.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public String getCityCode() {
+
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+}
